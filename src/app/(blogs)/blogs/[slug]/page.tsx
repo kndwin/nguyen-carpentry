74ccb365d4f407752d6531cc60f59469a19b4bce
kndwin <tnkevino@gmail.com>
2023-12-09 14:43:44 +1100

    wip: add a header

--- src/app/(blogs)/blogs/[slug]/page.tsx
+++ src/app/(blogs)/blogs/[slug]/page.tsx
@@ -66,47 +66,42 @@ export async function generateMetadata(params: Params): Promise<Metadata> {
 export default async function Blog(params: Params) {
   const blog = await getData(params);
   return (
-    <Layout>
-      <div className="max-w-6xl mx-auto px-5">
-        <Header />
-        <article className="mb-32">
-          <div className="relative mb-2 md:mb-4 sm:mx-0 w-full h-52 md:h-96">
-            <Image
-              alt={blog.title}
-              src={blog?.coverImage || ""}
-              fill
-              className="object-cover object-center"
-              priority
-            />
-          </div>
-          {Array.isArray(blog?.tags)
-            ? blog.tags.map(({ label }) => (
-                <span
-                  key="label"
-                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >
-                  {label}
-                </span>
-              ))
-            : null}
-          <h1 className="font-primary text-2xl font-bold md:text-4xl mb-2">
-            {blog.title}
-          </h1>
-          <div className="hidden md:block md:mb-12 text-slate-600">
-            Written on
-            <time>{format(parseISO(blog.publishedAt), "MMMM dd, yyyy")}</time>
-            {blog?.author?.name || ""}.
-          </div>
-          <hr className="border-neutral-200 mt-10 mb-10" />
-          <div className="max-w-2xl mx-auto">
-            <div
-              className="prose lg:prose-xl"
-              dangerouslySetInnerHTML={{ __html: blog.content }}
-            />
-          </div>
-        </article>
+    <Layout.Blog>
+      <div className="relative mb-2 md:mb-4 sm:mx-0 w-full h-52 md:h-96">
+        <Image
+          alt={blog.title}
+          src={blog?.coverImage || ""}
+          fill
+          className="object-cover object-center rounded-xl"
+          priority
+        />
       </div>
-    </Layout>
+      {Array.isArray(blog?.tags)
+        ? blog.tags.map(({ label }) => (
+            <span
+              key="label"
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >
+              {label}
+            </span>
+          ))
+        : null}
+      <h1 className="font-primary text-2xl font-bold md:text-4xl mb-2">
+        {blog.title}
+      </h1>
+      <div className="hidden md:block md:mb-12 text-slate-600">
+        Written on{" "}
+        <time>{format(parseISO(blog.publishedAt), "MMMM dd, yyyy")}</time>
+        <p>By {blog?.author?.name || ""}</p>
+      </div>
+      <hr className="border-neutral-200 mt-10 mb-10" />
+      <div className="max-w-2xl mx-auto">
+        <div
+          className="prose lg:prose-xl"
+          dangerouslySetInnerHTML={{ __html: blog.content }}
+        />
+      </div>
+    </Layout.Blog>
   );
 }
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,37 +1,57 @@
 import Link from "next/link";
 
-export const Layout = ({ children }: { children: React.ReactNode }) => {
+const LayoutDefault = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className="min-h-screen">
         <main>{children}</main>
       </div>
-      <Footer />
     </>
   );
 };
 
+const Blog = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Layout>
+      <div className="max-w-6xl mx-auto px-5">
+        <Header />
+        <article className="mb-32">{children}</article>
+      </div>
+    </Layout>
+  );
+};
+
+export const Layout = Object.assign(LayoutDefault, { Blog });
+
+const navigation = [{ title: "Blogs", path: "/blogs" }];
+
 export const Header = () => {
   return (
-    <nav className="layout flex items-center justify-between py-4">
-      <ul className="flex items-center justify-between space-x-3 text-xs md:space-x-4 md:text-base">
-        <li>
-          <Link href="/" className="hover:underline">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/#posts" className="hover:underline">
-            Posts
-          </Link>
-        </li>
-        <li>
-          <Link href="/#projects" className="hover:underline">
-            Projects
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="text-gray-1 body-font">
+      <div className="flex flex-wrap py-5 flex-col md:flex-row items-center">
+        <Link
+          href="/"
+          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">Nguyen Carpentry</span>
+        </Link>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <Link href="/blogs">Blogs</Link>
+        </nav>
+      </div>
+    </header>
   );
 };
 
