2d5938e7da2f36690bf2757a0d0f5a1b4c0d21d3
kndwin <tnkevino@gmail.com>
2023-12-09 18:55:02 +1100

    wip: blog item styling

--- src/app/(blogs)/blogs/page.tsx
+++ src/app/(blogs)/blogs/page.tsx
@@ -1,35 +1,89 @@
-import { getDocuments, load } from "outstatic/server";
+import Image from "next/image";
+import { getDocuments } from "outstatic/server";
 import { Layout } from "@/components/layout";
+import { parseISO, format } from "date-fns";
+import Link from "next/link";
 
-async function getData() {
-  const db = await load();
-  const blogs = await db.find({ collection: "blogs" }).toArray();
-  const documents = await getDocuments("blogs", [
-    "title",
-    "slug",
-    "description",
-  ]);
-
-  console.log({ documents });
-
-  return blogs;
-}
+const blogs = getDocuments("blogs", [
+  "title",
+  "slug",
+  "description",
+  "category",
+  "coverImage",
+]);
 
 export default async function Blogs() {
-  const blogs = await getData();
-
-  console.log({ blogs });
-
   return (
     <Layout.Default>
-      <h1 className="title-font sm:text-6xl text-5xl mb-4 font-bold">Blogs</h1>
       <ul>
         {blogs.map((blog) => (
           <li key={blog.slug}>
-            <a href={`/blogs/${blog.slug}`}>{blog.title}</a>
+            <Link href={`/blogs/${blog.slug}`}>
+              <BlogItem blog={blog as Blog} />
+            </Link>
           </li>
         ))}
       </ul>
     </Layout.Default>
   );
 }
+
+type Blog = (typeof blogs)[number] & {
+  category: { label: string; value: string }[];
+};
+
+export function BlogItem({ blog }: { blog: Blog }) {
+  return (
+    <div className="py-8 flex flex-wrap md:flex-nowrap">
+      <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+        <Image
+          width={200}
+          height={200}
+          src={blog.coverImage ?? ""}
+          alt={blog.title}
+          className="object-cover object-center rounded"
+        />
+      </div>
+      <div className="md:flex-grow">
+        <span className="mt-1 text-gray-10 text-sm mr-4">
+          {format(parseISO(blog.publishedAt), "MMMM dd, yyyy")}
+        </span>
+        <div className="inline-flex flex-wrap gap-2">
+          {blog.category.map(({ value }) => (
+            <span
+              key={value}
+              className="capitalize bg-yellow-5 rounded w-fit text-xs px-1 font-medium text-yellow-12"
+            >
+              {value}
+            </span>
+          ))}
+        </div>
+        <h2 className="text-2xl text-gray-12 font-bold title-font mb-2">
+          {blog.title}
+        </h2>
+        <p className="leading-relaxed">{blog.description}</p>
+        <a className="text-yellow-10 inline-flex items-center mt-4 line-clamp-2 overflow-ellipsis">
+          Learn More
+          <Icons.RightArrow />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+const Icons = {
+  RightArrow: () => (
+    <svg
+      className="w-4 h-4 ml-2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M5 12h14"></path>
+      <path d="M12 5l7 7-7 7"></path>
+    </svg>
+  ),
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -194,27 +194,6 @@ function Blogs() {
         <div className="-my-8 divide-y-2 divide-gray-100">
           <div className="py-8 flex flex-wrap md:flex-nowrap">
             <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <span className="font-semibold title-font">CATEGORY</span>
-              <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-            </div>
-            <div className="md:flex-grow">
-              <h2 className="text-2xl font-medium text-gray-11 title-font mb-2">
-                Bitters hashtag waistcoat fashion axe chia unicorn
-              </h2>
-              <p className="leading-relaxed">
-                Glossier echo park pug, church-key sartorial biodiesel
-                vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon
-                party messenger bag selfies, poke vaporware kombucha
-                lumbersexual pork belly polaroid hoodie portland craft beer.
-              </p>
-              <a className="text-yellow-500 inline-flex items-center mt-4">
-                Learn More
-                <Icons.RightArrow />
-              </a>
-            </div>
-          </div>
-          <div className="py-8 flex flex-wrap md:flex-nowrap">
-            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
               <span className="font-semibold title-font text-gray-700">
                 CATEGORY
               </span>
@@ -232,7 +211,6 @@ function Blogs() {
               </p>
               <a className="text-indigo-500 inline-flex items-center mt-4">
                 Learn More
-                <Icons.RightArrow />
               </a>
             </div>
           </div>
@@ -255,7 +233,6 @@ function Blogs() {
               </p>
               <a className="text-indigo-500 inline-flex items-center mt-4">
                 Learn More
-                <Icons.RightArrow />
               </a>
             </div>
           </div>
@@ -264,20 +241,3 @@ function Blogs() {
     </section>
   );
 }
-
-const Icons = {
-  RightArrow: () => (
-    <svg
-      className="w-4 h-4 ml-2"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth="2"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M5 12h14"></path>
-      <path d="M12 5l7 7-7 7"></path>
-    </svg>
-  ),
-};
